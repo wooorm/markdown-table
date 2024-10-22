6dab0baef69c0b5f14058f6a0df59f2969b54508
Titus Wormer <tituswormer@gmail.com>
2024-10-22 13:54:43 +0200

    Refactor code-style

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+// To do: next major: remove.
+/**
+ * @typedef {Options} MarkdownTableOptions
+ *   Configuration.
+ */
+
 /**
  * @typedef Options
- *   Configuration (optional).
- * @property {string|null|ReadonlyArray<string|null|undefined>} [align]
- *   One style for all columns, or styles for their respective columns.
- *   Each style is either `'l'` (left), `'r'` (right), or `'c'` (center).
- *   Other values are treated as `''`, which doesn’t place the colon in the
- *   alignment row but does align left.
- *   *Only the lowercased first character is used, so `Right` is fine.*
- * @property {boolean} [padding=true]
- *   Whether to add a space of padding between delimiters and cells.
- *
- *   When `true`, there is padding:
+ *   Configuration.
+ * @property {boolean | null | undefined} [alignDelimiters=true]
+ *   Whether to align the delimiters (default: `true`);
+ *   they are aligned by default:
  *
  *   ```markdown
  *   | Alpha | B     |
@@ -18,36 +17,22 @@
  *   | C     | Delta |
  *   ```
  *
- *   When `false`, there is no padding:
+ *   Pass `false` to make them staggered:
  *
  *   ```markdown
- *   |Alpha|B    |
- *   |-----|-----|
- *   |C    |Delta|
+ *   | Alpha | B |
+ *   | - | - |
+ *   | C | Delta |
  *   ```
- * @property {boolean} [delimiterStart=true]
- *   Whether to begin each row with the delimiter.
- *
- *   > 👉 **Note**: please don’t use this: it could create fragile structures
- *   > that aren’t understandable to some markdown parsers.
- *
- *   When `true`, there are starting delimiters:
- *
- *   ```markdown
- *   | Alpha | B     |
- *   | ----- | ----- |
- *   | C     | Delta |
- *   ```
- *
- *   When `false`, there are no starting delimiters:
- *
- *   ```markdown
- *   Alpha | B     |
- *   ----- | ----- |
- *   C     | Delta |
- *   ```
- * @property {boolean} [delimiterEnd=true]
- *   Whether to end each row with the delimiter.
+ * @property {ReadonlyArray<string | null | undefined> | string | null | undefined} [align]
+ *   How to align columns (default: `''`);
+ *   one style for all columns or styles for their respective columns;
+ *   each style is either `'l'` (left), `'r'` (right), or `'c'` (center);
+ *   other values are treated as `''`, which doesn’t place the colon in the
+ *   alignment row but does align left;
+ *   *only the lowercased first character is used, so `Right` is fine.*
+ * @property {boolean | null | undefined} [delimiterEnd=true]
+ *   Whether to end each row with the delimiter (default: `true`).
  *
  *   > 👉 **Note**: please don’t use this: it could create fragile structures
  *   > that aren’t understandable to some markdown parsers.
@@ -67,9 +52,13 @@
  *   | ----- | -----
  *   | C     | Delta
  *   ```
- * @property {boolean} [alignDelimiters=true]
- *   Whether to align the delimiters.
- *   By default, they are aligned:
+ * @property {boolean | null | undefined} [delimiterStart=true]
+ *   Whether to begin each row with the delimiter (default: `true`).
+ *
+ *   > 👉 **Note**: please don’t use this: it could create fragile structures
+ *   > that aren’t understandable to some markdown parsers.
+ *
+ *   When `true`, there are starting delimiters:
  *
  *   ```markdown
  *   | Alpha | B     |
@@ -77,21 +66,40 @@
  *   | C     | Delta |
  *   ```
  *
- *   Pass `false` to make them staggered:
+ *   When `false`, there are no starting delimiters:
  *
  *   ```markdown
- *   | Alpha | B |
- *   | - | - |
- *   | C | Delta |
+ *   Alpha | B     |
+ *   ----- | ----- |
+ *   C     | Delta |
  *   ```
- * @property {(value: string) => number} [stringLength]
- *   Function to detect the length of table cell content.
- *   This is used when aligning the delimiters (`|`) between table cells.
- *   Full-width characters and emoji mess up delimiter alignment when viewing
- *   the markdown source.
- *   To fix this, you can pass this function, which receives the cell content
- *   and returns its “visible” size.
- *   Note that what is and isn’t visible depends on where the text is displayed.
+ * @property {boolean | null | undefined} [padding=true]
+ *   Whether to add a space of padding between delimiters and cells
+ *   (default: `true`).
+ *
+ *   When `true`, there is padding:
+ *
+ *   ```markdown
+ *   | Alpha | B     |
+ *   | ----- | ----- |
+ *   | C     | Delta |
+ *   ```
+ *
+ *   When `false`, there is no padding:
+ *
+ *   ```markdown
+ *   |Alpha|B    |
+ *   |-----|-----|
+ *   |C    |Delta|
+ *   ```
+ * @property {((value: string) => number) | null | undefined} [stringLength]
+ *   Function to detect the length of table cell content (optional);
+ *   this is used when aligning the delimiters (`|`) between table cells;
+ *   full-width characters and emoji mess up delimiter alignment when viewing
+ *   the markdown source;
+ *   to fix this, you can pass this function,
+ *   which receives the cell content and returns its “visible” size;
+ *   note that what is and isn’t visible depends on where the text is displayed.
  *
  *   Without such a function, the following:
  *
@@ -138,23 +146,32 @@
  */
 
 /**
- * @typedef {Options} MarkdownTableOptions
- * @todo
- *   Remove next major.
+ * @param {string} value
+ *   Cell value.
+ * @returns {number}
+ *   Cell size.
  */
+function defaultStringLength(value) {
+  return value.length
+}
 
 /**
- * Generate a markdown ([GFM](https://docs.github.com/en/github/writing-on-github/working-with-advanced-formatting/organizing-information-with-tables)) table..
+ * Generate a markdown
+ * ([GFM](https://docs.github.com/en/github/writing-on-github/working-with-advanced-formatting/organizing-information-with-tables))
+ * table.
  *
- * @param {ReadonlyArray<ReadonlyArray<string|null|undefined>>} table
+ * @param {ReadonlyArray<ReadonlyArray<string | null | undefined>>} table
  *   Table data (matrix of strings).
- * @param {Options} [options]
+ * @param {Readonly<Options> | null | undefined} [options]
  *   Configuration (optional).
  * @returns {string}
+ *   Result.
  */
-export function markdownTable(table, options = {}) {
-  const align = (options.align || []).concat()
-  const stringLength = options.stringLength || defaultStringLength
+export function markdownTable(table, options) {
+  const settings = options || {}
+  // To do: next major: change to spread.
+  const align = (settings.align || []).concat()
+  const stringLength = settings.stringLength || defaultStringLength
   /** @type {Array<number>} Character codes as symbols for alignment per column. */
   const alignments = []
   /** @type {Array<Array<string>>} Cells per row. */
@@ -182,7 +199,7 @@ export function markdownTable(table, options = {}) {
     while (++columnIndex < table[rowIndex].length) {
       const cell = serialize(table[rowIndex][columnIndex])
 
-      if (options.alignDelimiters !== false) {
+      if (settings.alignDelimiters !== false) {
         const size = stringLength(cell)
         sizes[columnIndex] = size
 
@@ -239,7 +256,7 @@ export function markdownTable(table, options = {}) {
 
     // There *must* be at least one hyphen-minus in each alignment cell.
     let size =
-      options.alignDelimiters === false
+      settings.alignDelimiters === false
         ? 1
         : Math.max(
             1,
@@ -248,7 +265,7 @@ export function markdownTable(table, options = {}) {
 
     const cell = before + '-'.repeat(size) + after
 
-    if (options.alignDelimiters !== false) {
+    if (settings.alignDelimiters !== false) {
       size = before.length + size + after.length
 
       if (size > longestCellByColumn[columnIndex]) {
@@ -281,7 +298,7 @@ export function markdownTable(table, options = {}) {
       let before = ''
       let after = ''
 
-      if (options.alignDelimiters !== false) {
+      if (settings.alignDelimiters !== false) {
         const size =
           longestCellByColumn[columnIndex] - (sizes[columnIndex] || 0)
         const code = alignments[columnIndex]
@@ -301,36 +318,36 @@ export function markdownTable(table, options = {}) {
         }
       }
 
-      if (options.delimiterStart !== false && !columnIndex) {
+      if (settings.delimiterStart !== false && !columnIndex) {
         line.push('|')
       }
 
       if (
-        options.padding !== false &&
+        settings.padding !== false &&
         // Don’t add the opening space if we’re not aligning and the cell is
         // empty: there will be a closing space.
-        !(options.alignDelimiters === false && cell === '') &&
-        (options.delimiterStart !== false || columnIndex)
+        !(settings.alignDelimiters === false && cell === '') &&
+        (settings.delimiterStart !== false || columnIndex)
       ) {
         line.push(' ')
       }
 
-      if (options.alignDelimiters !== false) {
+      if (settings.alignDelimiters !== false) {
         line.push(before)
       }
 
       line.push(cell)
 
-      if (options.alignDelimiters !== false) {
+      if (settings.alignDelimiters !== false) {
         line.push(after)
       }
 
-      if (options.padding !== false) {
+      if (settings.padding !== false) {
         line.push(' ')
       }
 
       if (
-        options.delimiterEnd !== false ||
+        settings.delimiterEnd !== false ||
         columnIndex !== mostCellsPerRow - 1
       ) {
         line.push('|')
@@ -338,7 +355,7 @@ export function markdownTable(table, options = {}) {
     }
 
     lines.push(
-      options.delimiterEnd === false
+      settings.delimiterEnd === false
         ? line.join('').replace(/ +$/, '')
         : line.join('')
     )
@@ -348,24 +365,20 @@ export function markdownTable(table, options = {}) {
 }
 
 /**
- * @param {string|null|undefined} [value]
+ * @param {string | null | undefined} [value]
+ *   Value to serialize.
  * @returns {string}
+ *   Result.
  */
 function serialize(value) {
   return value === null || value === undefined ? '' : String(value)
 }
 
 /**
- * @param {string} value
+ * @param {string | null | undefined} value
+ *   Value.
  * @returns {number}
- */
-function defaultStringLength(value) {
-  return value.length
-}
-
-/**
- * @param {string|null|undefined} value
- * @returns {number}
+ *   Alignment.
  */
 function toAlignment(value) {
   const code = typeof value === 'string' ? value.codePointAt(0) : 0
--- test.js
+++ test.js
@@ -4,293 +4,333 @@ import chalk from 'chalk'
 import strip from 'strip-ansi'
 import {markdownTable} from './index.js'
 
-test('markdownTable()', () => {
-  assert.equal(
-    markdownTable([
-      ['Branch', 'Commit'],
-      ['main', '0123456789abcdef'],
-      ['staging', 'fedcba9876543210']
-    ]),
-    [
-      '| Branch  | Commit           |',
-      '| ------- | ---------------- |',
-      '| main    | 0123456789abcdef |',
-      '| staging | fedcba9876543210 |'
-    ].join('\n'),
-    'should create a table'
-  )
+test('markdownTable()', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'markdownTable'
+    ])
+  })
 
-  assert.equal(
-    markdownTable([
-      ['Type', 'Value'],
-      ['string', 'alpha'],
-      // @ts-ignore
-      ['number', 1],
-      // @ts-ignore
-      ['boolean', true],
-      ['undefined', undefined],
-      ['null', null],
-      // @ts-ignore
-      ['Array', [1, 2, 3]]
-    ]),
-    [
-      '| Type      | Value |',
-      '| --------- | ----- |',
-      '| string    | alpha |',
-      '| number    | 1     |',
-      '| boolean   | true  |',
-      '| undefined |       |',
-      '| null      |       |',
-      '| Array     | 1,2,3 |'
-    ].join('\n'),
-    'should serialize values'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
-        ['A', 'B', 'C'],
-        ['a', 'b', 'c'],
-        ['a', 'b'],
-        ['a'],
-        [],
-        ['a', 'b', ''],
-        ['', 'b', 'c'],
-        ['a', '', ''],
-        ['', '', 'c'],
-        ['', '', '']
-      ],
-      {align: 'c'}
-    ),
-    [
-      '|  A  |  B  |  C  |',
-      '| :-: | :-: | :-: |',
-      '|  a  |  b  |  c  |',
-      '|  a  |  b  |     |',
-      '|  a  |     |     |',
-      '|     |     |     |',
-      '|  a  |  b  |     |',
-      '|     |  b  |  c  |',
-      '|  a  |     |     |',
-      '|     |     |  c  |',
-      '|     |     |     |'
-    ].join('\n'),
-    'should work correctly when cells are missing'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
-        ['Beep', 'No.'],
-        ['boop', '33450'],
-        ['foo', '1006'],
-        ['bar', '45']
-      ],
-      {align: ['l', 'r']}
-    ),
-    [
-      '| Beep |   No. |',
-      '| :--- | ----: |',
-      '| boop | 33450 |',
-      '| foo  |  1006 |',
-      '| bar  |    45 |'
-    ].join('\n'),
-    'should align left and right'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
-        ['Beep', 'No.', 'Boop'],
-        ['beep', '1024', 'xyz'],
-        ['boop', '3388450', 'tuv'],
-        ['foo', '10106', 'qrstuv'],
-        ['bar', '45', 'lmno']
-      ],
-      {align: ['l', 'c', 'l']}
-    ),
-    [
-      '| Beep |   No.   | Boop   |',
-      '| :--- | :-----: | :----- |',
-      '| beep |   1024  | xyz    |',
-      '| boop | 3388450 | tuv    |',
-      '| foo  |  10106  | qrstuv |',
-      '| bar  |    45   | lmno   |'
-    ].join('\n'),
-    'should align center'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
-        ['Very long', 'Even longer'],
-        ['boop', '33450'],
-        ['foo', '1006'],
-        ['bar', '45']
-      ],
-      {align: 'c'}
-    ),
-    [
-      '| Very long | Even longer |',
-      '| :-------: | :---------: |',
-      '|    boop   |    33450    |',
-      '|    foo    |     1006    |',
-      '|    bar    |      45     |'
-    ].join('\n'),
-    'should accept a single value'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
-        ['Beep', 'No.', 'Boop'],
-        ['beep', '1024', 'xyz'],
-        ['boop', '3388450', 'tuv'],
-        ['foo', '10106', 'qrstuv'],
-        ['bar', '45', 'lmno']
-      ],
-      {align: ['left', 'center', 'right']}
-    ),
-    [
-      '| Beep |   No.   |   Boop |',
-      '| :--- | :-----: | -----: |',
-      '| beep |   1024  |    xyz |',
-      '| boop | 3388450 |    tuv |',
-      '| foo  |  10106  | qrstuv |',
-      '| bar  |    45   |   lmno |'
-    ].join('\n'),
-    'should accept multi-character values'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
+  await t.test('should create a table', async function () {
+    assert.equal(
+      markdownTable([
         ['Branch', 'Commit'],
         ['main', '0123456789abcdef'],
         ['staging', 'fedcba9876543210']
-      ],
-      {padding: false}
-    ),
-    [
-      '|Branch |Commit          |',
-      '|-------|----------------|',
-      '|main   |0123456789abcdef|',
-      '|staging|fedcba9876543210|'
-    ].join('\n'),
-    'should create a table without padding'
-  )
-
-  assert.equal(
-    markdownTable(
+      ]),
       [
-        ['Branch', 'Commit', 'Beep', 'No.', 'Boop'],
-        ['main', '0123456789abcdef', 'beep', '1024', 'xyz'],
-        ['staging', 'fedcba9876543210', 'boop', '3388450', 'tuv']
-      ],
-      {alignDelimiters: false, align: ['', 'l', 'c', 'r']}
-    ),
-    [
-      '| Branch | Commit | Beep | No. | Boop |',
-      '| - | :- | :-: | -: | - |',
-      '| main | 0123456789abcdef | beep | 1024 | xyz |',
-      '| staging | fedcba9876543210 | boop | 3388450 | tuv |'
-    ].join('\n'),
-    'should create a table without aligned delimiters'
-  )
+        '| Branch  | Commit           |',
+        '| ------- | ---------------- |',
+        '| main    | 0123456789abcdef |',
+        '| staging | fedcba9876543210 |'
+      ].join('\n')
+    )
+  })
 
-  assert.equal(
-    markdownTable(
+  await t.test('should serialize values', async function () {
+    assert.equal(
+      markdownTable([
+        ['Type', 'Value'],
+        ['string', 'alpha'],
+        // @ts-expect-error: check handling of primitives.
+        ['number', 1],
+        // @ts-expect-error: check handling of primitives.
+        ['boolean', true],
+        ['undefined', undefined],
+        ['null', null],
+        // @ts-expect-error: check handling of other values.
+        ['Array', [1, 2, 3]]
+      ]),
       [
-        ['A'],
-        ['', '0123456789abcdef'],
-        ['staging', 'fedcba9876543210'],
-        ['develop']
-      ],
-      {alignDelimiters: false}
-    ),
-    [
-      '| A | |',
-      '| - | - |',
-      '| | 0123456789abcdef |',
-      '| staging | fedcba9876543210 |',
-      '| develop | |'
-    ].join('\n'),
-    'handles short rules and missing elements for tables w/o aligned delimiters'
+        '| Type      | Value |',
+        '| --------- | ----- |',
+        '| string    | alpha |',
+        '| number    | 1     |',
+        '| boolean   | true  |',
+        '| undefined |       |',
+        '| null      |       |',
+        '| Array     | 1,2,3 |'
+      ].join('\n')
+    )
+  })
+
+  await t.test(
+    'should work correctly when cells are missing',
+    async function () {
+      assert.equal(
+        markdownTable(
+          [
+            ['A', 'B', 'C'],
+            ['a', 'b', 'c'],
+            ['a', 'b'],
+            ['a'],
+            [],
+            ['a', 'b', ''],
+            ['', 'b', 'c'],
+            ['a', '', ''],
+            ['', '', 'c'],
+            ['', '', '']
+          ],
+          {align: 'c'}
+        ),
+        [
+          '|  A  |  B  |  C  |',
+          '| :-: | :-: | :-: |',
+          '|  a  |  b  |  c  |',
+          '|  a  |  b  |     |',
+          '|  a  |     |     |',
+          '|     |     |     |',
+          '|  a  |  b  |     |',
+          '|     |  b  |  c  |',
+          '|  a  |     |     |',
+          '|     |     |  c  |',
+          '|     |     |     |'
+        ].join('\n')
+      )
+    }
   )
 
-  assert.equal(
-    markdownTable(
-      [
-        ['Branch', 'Commit'],
-        ['main', '0123456789abcdef'],
-        ['staging', 'fedcba9876543210'],
-        ['develop']
-      ],
-      {delimiterStart: false}
-    ),
-    [
-      'Branch  | Commit           |',
-      '------- | ---------------- |',
-      'main    | 0123456789abcdef |',
-      'staging | fedcba9876543210 |',
-      'develop |                  |'
-    ].join('\n'),
-    'should create rows without starting delimiter'
-  )
-
-  assert.equal(
-    markdownTable(
-      [
-        ['Branch', 'Commit'],
-        ['main', '0123456789abcdef'],
-        ['staging', 'fedcba9876543210'],
-        ['develop']
-      ],
-      {delimiterEnd: false}
-    ),
-    [
-      '| Branch  | Commit',
-      '| ------- | ----------------',
-      '| main    | 0123456789abcdef',
-      '| staging | fedcba9876543210',
-      '| develop |'
-    ].join('\n'),
-    'should create rows without ending delimiter'
-  )
-
-  assert.equal(
-    strip(
+  await t.test('should align left and right', async function () {
+    assert.equal(
       markdownTable(
         [
-          ['A', 'B', 'C'],
-          [chalk.red('Red'), chalk.green('Green'), chalk.blue('Blue')],
-          [chalk.bold('Bold'), chalk.underline(''), chalk.italic('Italic')],
-          [
-            chalk.inverse('Inverse'),
-            chalk.strikethrough('Strike'),
-            chalk.hidden('Hidden')
-          ],
+          ['Beep', 'No.'],
+          ['boop', '33450'],
+          ['foo', '1006'],
+          ['bar', '45']
+        ],
+        {align: ['l', 'r']}
+      ),
+      [
+        '| Beep |   No. |',
+        '| :--- | ----: |',
+        '| boop | 33450 |',
+        '| foo  |  1006 |',
+        '| bar  |    45 |'
+      ].join('\n')
+    )
+  })
+
+  await t.test('should align center', async function () {
+    assert.equal(
+      markdownTable(
+        [
+          ['Beep', 'No.', 'Boop'],
+          ['beep', '1024', 'xyz'],
+          ['boop', '3388450', 'tuv'],
+          ['foo', '10106', 'qrstuv'],
           ['bar', '45', 'lmno']
         ],
-        {align: ['', 'c', 'r'], stringLength}
+        {align: ['l', 'c', 'l']}
+      ),
+      [
+        '| Beep |   No.   | Boop   |',
+        '| :--- | :-----: | :----- |',
+        '| beep |   1024  | xyz    |',
+        '| boop | 3388450 | tuv    |',
+        '| foo  |  10106  | qrstuv |',
+        '| bar  |    45   | lmno   |'
+      ].join('\n')
+    )
+  })
+
+  await t.test('should accept a single value', async function () {
+    assert.equal(
+      markdownTable(
+        [
+          ['Very long', 'Even longer'],
+          ['boop', '33450'],
+          ['foo', '1006'],
+          ['bar', '45']
+        ],
+        {align: 'c'}
+      ),
+      [
+        '| Very long | Even longer |',
+        '| :-------: | :---------: |',
+        '|    boop   |    33450    |',
+        '|    foo    |     1006    |',
+        '|    bar    |      45     |'
+      ].join('\n')
+    )
+  })
+
+  await t.test('should accept multi-character values', async function () {
+    assert.equal(
+      markdownTable(
+        [
+          ['Beep', 'No.', 'Boop'],
+          ['beep', '1024', 'xyz'],
+          ['boop', '3388450', 'tuv'],
+          ['foo', '10106', 'qrstuv'],
+          ['bar', '45', 'lmno']
+        ],
+        {align: ['left', 'center', 'right']}
+      ),
+      [
+        '| Beep |   No.   |   Boop |',
+        '| :--- | :-----: | -----: |',
+        '| beep |   1024  |    xyz |',
+        '| boop | 3388450 |    tuv |',
+        '| foo  |  10106  | qrstuv |',
+        '| bar  |    45   |   lmno |'
+      ].join('\n')
+    )
+  })
+
+  await t.test('should create a table without padding', async function () {
+    assert.equal(
+      markdownTable(
+        [
+          ['Branch', 'Commit'],
+          ['main', '0123456789abcdef'],
+          ['staging', 'fedcba9876543210']
+        ],
+        {padding: false}
+      ),
+      [
+        '|Branch |Commit          |',
+        '|-------|----------------|',
+        '|main   |0123456789abcdef|',
+        '|staging|fedcba9876543210|'
+      ].join('\n')
+    )
+  })
+
+  await t.test(
+    'should create a table without aligned delimiters',
+    async function () {
+      assert.equal(
+        markdownTable(
+          [
+            ['Branch', 'Commit', 'Beep', 'No.', 'Boop'],
+            ['main', '0123456789abcdef', 'beep', '1024', 'xyz'],
+            ['staging', 'fedcba9876543210', 'boop', '3388450', 'tuv']
+          ],
+          {alignDelimiters: false, align: ['', 'l', 'c', 'r']}
+        ),
+        [
+          '| Branch | Commit | Beep | No. | Boop |',
+          '| - | :- | :-: | -: | - |',
+          '| main | 0123456789abcdef | beep | 1024 | xyz |',
+          '| staging | fedcba9876543210 | boop | 3388450 | tuv |'
+        ].join('\n')
       )
-    ),
-    [
-      '| A       |    B   |      C |',
-      '| ------- | :----: | -----: |',
-      '| Red     |  Green |   Blue |',
-      '| Bold    |        | Italic |',
-      '| Inverse | Strike | Hidden |',
-      '| bar     |   45   |   lmno |'
-    ].join('\n'),
-    'should use `stringLength` to detect cell lengths'
+    }
+  )
+
+  await t.test(
+    'should handle short rules and missing elements for tables w/o aligned delimiters',
+    async function () {
+      assert.equal(
+        markdownTable(
+          [
+            ['A'],
+            ['', '0123456789abcdef'],
+            ['staging', 'fedcba9876543210'],
+            ['develop']
+          ],
+          {alignDelimiters: false}
+        ),
+        [
+          '| A | |',
+          '| - | - |',
+          '| | 0123456789abcdef |',
+          '| staging | fedcba9876543210 |',
+          '| develop | |'
+        ].join('\n')
+      )
+    }
+  )
+
+  await t.test(
+    'should create rows without starting delimiter',
+    async function () {
+      assert.equal(
+        markdownTable(
+          [
+            ['Branch', 'Commit'],
+            ['main', '0123456789abcdef'],
+            ['staging', 'fedcba9876543210'],
+            ['develop']
+          ],
+          {delimiterStart: false}
+        ),
+        [
+          'Branch  | Commit           |',
+          '------- | ---------------- |',
+          'main    | 0123456789abcdef |',
+          'staging | fedcba9876543210 |',
+          'develop |                  |'
+        ].join('\n')
+      )
+    }
+  )
+
+  await t.test(
+    'should create rows without ending delimiter',
+    async function () {
+      assert.equal(
+        markdownTable(
+          [
+            ['Branch', 'Commit'],
+            ['main', '0123456789abcdef'],
+            ['staging', 'fedcba9876543210'],
+            ['develop']
+          ],
+          {delimiterEnd: false}
+        ),
+        [
+          '| Branch  | Commit',
+          '| ------- | ----------------',
+          '| main    | 0123456789abcdef',
+          '| staging | fedcba9876543210',
+          '| develop |'
+        ].join('\n')
+      )
+    }
+  )
+
+  await t.test(
+    'should use `stringLength` to detect cell lengths',
+    async function () {
+      assert.equal(
+        strip(
+          markdownTable(
+            [
+              ['A', 'B', 'C'],
+              [chalk.red('Red'), chalk.green('Green'), chalk.blue('Blue')],
+              [chalk.bold('Bold'), chalk.underline(''), chalk.italic('Italic')],
+              [
+                chalk.inverse('Inverse'),
+                chalk.strikethrough('Strike'),
+                chalk.hidden('Hidden')
+              ],
+              ['bar', '45', 'lmno']
+            ],
+            {align: ['', 'c', 'r'], stringLength}
+          )
+        ),
+        [
+          '| A       |    B   |      C |',
+          '| ------- | :----: | -----: |',
+          '| Red     |  Green |   Blue |',
+          '| Bold    |        | Italic |',
+          '| Inverse | Strike | Hidden |',
+          '| bar     |   45   |   lmno |'
+        ].join('\n')
+      )
+    }
   )
 })
 
 /**
  * Get the length of a string, minus ANSI color characters.
+ *
  * @param {string} value
+ *   Cell value.
  * @returns {number}
+ *   Cell size.
  */
 function stringLength(value) {
   return strip(value).length
